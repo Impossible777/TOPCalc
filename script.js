a1a4f846d6387c17ce5dc22b508981bc67dd0d40
Impossible777 <cristmichael7@gmail.com>
2023-08-24 16:42:46 -0500

    Added the ability to calculate single digit numbers

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var input1
-var input2
-var operator
+var input2 
+var operator 
+let count = 1;
+const clickButton = document.getElementsByClassName('numbers');
+const display = document.getElementById('display');
+const clear = document.getElementById('clear')
+const equals = document.getElementById('equals')
+const answer = document.getElementById("answer")
 
 
 
@@ -30,17 +36,52 @@ function operate(a, b, perform) {
     } else if (perform == "/") {
         return divide(a,b)
     } else {
-        return "Not a valied operator"
+        return "Not a valid operator"
+
     }
 }
 
-const clickButton = document.getElementsByClassName('numbers');
 
-for (let i = 0; i <clickButton.length; i++) {
-    clickButton[i].addEventListener("click", function() {
-        console.log("Clicked!")
-    })
+function clearData() {
+    display.textContent = ""
+    answer.textContent = "";
+    
+    input1 = 1;
+    input2 = 1;
+    
 }
+
+
+function identifyNumber () {
+    const content = this.textContent
+    display.textContent = display.textContent + content
+    if (count === 1) {
+        input1 = parseInt(content)
+        count = count + 1
+    } else if (count === 2) {
+        operator = content
+        count = count + 1
+    } else if (count === 3) {
+        input2 = parseInt(content);
+        count = 1
+    }
+  
+
+}
+
+for (let i =0; i < clickButton.length; i++) {
+    clickButton[i].addEventListener("click", identifyNumber)
+}
+
+clear.addEventListener('click', clearData);
+equals.addEventListener('click', function() {
+    console.log(input1)
+    console.log(input2)
+    console.log(operator)
+    const result = operate(input1, input2, operator)
+    display.textContent = display.textContent + " " + result
+})
+
 
 
 
