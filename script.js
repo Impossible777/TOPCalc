48095a2ae7eda3fe5f3b102f35c1812496167568
Impossible777 <cristmichael7@gmail.com>
2023-08-24 12:43:45 -0500

    Created visual display of calc.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var input1
+var input2
+var operator
+
+
+
 function add(a, b) {
     return a + b
 }
@@ -6,11 +12,38 @@ function subtract(a, b) {
     return a - b
 }
 
-function multiple(a, b) {
+function multiply(a, b) {
     return a * b
 }
 
 function divide(a, b) {
     return a / b
 }
+
+function operate(a, b, perform) {
+    if (perform == "+") {
+        return add(a,b)
+    } else if (perform == "-") {
+        return subtract(a,b)
+    } else if (perform == "*") {
+        return multiply(a,b)
+    } else if (perform == "/") {
+        return divide(a,b)
+    } else {
+        return "Not a valied operator"
+    }
+}
+
+const clickButton = document.getElementsByClassName('numbers');
+
+for (let i = 0; i <clickButton.length; i++) {
+    clickButton[i].addEventListener("click", function() {
+        console.log("Clicked!")
+    })
+}
+
+
+
+
+
 
